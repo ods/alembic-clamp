76bf8616a051fd48175c498b6f1445b326b56861
deniallugo <l.daniil@codewizard.team>
2020-05-22 11:53:55 +0300

    feat(setup): Initialize setup script with all requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,33 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
+install_requires = [
+    'alembic>=1.4.1',
+    'asyncpg>=0.20.1',
+    'asyncpgsa>=0.26.1',
+    'psycopg2-binary>=2.7.7',
+    'click>=7.0'
+    'SQLAlchemy>=1.3.15',
+]
+
+setup_requires = [
+    'setuptools_scm>=3.3.3',
+]
+
+tests_require = [
+    'pytest>=5.3.5',
+    'pytest-cov>=2.8.1',
+]
 
 setup(
-    use_scm_version = True,
+    name='alembic_clamp',
+    use_scm_version=True,
+    description='A wrapper around alembic (SQLAlchemy migration tool), that is configurable from code instead of alembic.ini file.',
+    packages=find_packages(include=['alembic_clamp', 'alembic_clamp.*']),
+    include_package_data=True,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    extras_require={
+        'test': tests_require,
+    },
 )
